fdfc48733aaae5b7896bb78d95618d22626ef0ff
Gourav Dutta <108947265+GouravDutta-01@users.noreply.github.com>
2024-08-08 00:03:26 +0530

    Day 24

---
+++ Greedy/furthest-building-you-can-reach_1642.java
@@ -0,0 +1,33 @@
+// Question Link :
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int i;
+        for(i=0; i<heights.length-1; i++){
+            if(heights[i+1] <= heights[i]){
+                continue;
+            }
+            int diff = heights[i+1]-heights[i];
+            if(bricks >= diff){
+                bricks -= diff;
+                pq.add(diff);
+            }
+            else if(ladders > 0){
+                if(!pq.isEmpty()){
+                    int pastMaxBricks = pq.peek();
+                    if(pastMaxBricks > diff){
+                        bricks += pastMaxBricks;
+                        pq.poll();
+                        bricks -= diff;
+                        pq.add(diff);
+                    }
+                }
+                ladders--;
+            }
+            else{
+                break;
+            }
+        }
+        return i;   
+    }
+}
